aca1efd3e1aceb75bb26a0a39f997ad80c91c0ad
Nanditha. U <74827999+Nanditha163@users.noreply.github.com>
2021-04-04 15:40:01 +0530

    Update Nanditha.U-212218104117.cpp

--- Nanditha.U-212218104117.cpp
+++ Nanditha.U-212218104117.cpp
@@ -28,12 +28,6 @@ void MainWindow::on_pushButton_clicked()
     QMessageBox::StandardButton reply= QMessageBox::information(this,"Nanditha","212218104117 SUCCESSFULLY COMPLETED");
 }
 
-void MainWindow::on_lineEdit_8_textChanged(const QString &link)
-{
-    ui->lineEdit->setValidator(new QIntValidator(50,100,ui->lineEdit));
-
-}
-
 
 void MainWindow::on_radioButton_clicked()
 {
@@ -47,4 +41,16 @@ void MainWindow::on_radioButton_clicked()
     qDebug() <<group.checkedId();
     qDebug() <<group.checkedButton();
 }
+
+void MainWindow::on_lineEdit_8_returnPressed()
+{
+    QRegularExpression re("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+           QRegularExpressionMatch match = re.match(ui->lineEdit_3->text());
+           bool hasMatch = match.hasMatch();
+           if (!hasMatch)
+           {
+              QMessageBox::information(this,"Nanditha","Invalid entry !");
+           }
+}
+
 
